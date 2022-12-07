47bbf796d06bd1258f0d54bf6fb5c54733ebbdfd
unknown <jahandarmv@code.edu.az>
2022-12-07 17:22:33 +0400

    firs task

---
+++ Alqoritms/Program.cs
@@ -0,0 +1,97 @@
+﻿//1) Verilmiş n - ədədinin 3-ə və 7-ə bölünüb-bölünməməsini tapın.
+
+
+//int n = 21;
+//if (n%3==0)
+
+//{
+
+
+//}
+//if (n%7==0)
+//{
+//    Console.WriteLine("Bolunur");
+//}
+//else
+//{
+//    Console.WriteLine("Bolunmur");
+//}
+
+
+
+
+//2) n və m ədədləri verilir. Əgər n və m ədədləri hər ikisi cütdürsə n və m ədədlərinin cəmini hesablayın.
+//using System;
+
+//int n = 6;
+//int m = 8;
+//int result = 0;
+//if (n % 2 == 0)
+//{
+
+//}
+//if (m % 2 == 0)
+//{
+//    result = n + m;
+//    Console.WriteLine(result);
+//}
+//else
+//{
+//    Console.WriteLine("duz deyil");
+//}
+
+
+
+
+//  3) Verilmis n ve m (n<m) ededleri arasindaki tek ededlerin cemini tapin.
+
+//int n = 3;
+//int m = 9;
+//int count = 0;
+//for (int i = n; i <= m; i++)
+//{
+//    if (i % 2 == 1)
+
+//    {
+//        count = count + i;
+
+//    }
+//}
+
+//Console.WriteLine(count);
+
+
+
+
+//4) Verilmish arrayin icindeki tek ededlerin cemini tapin.
+
+//int[] nums = { 1, 2, 3, 4, 5, 6, 10, 11 };
+//int sum = 0;
+//for (int i = 0; i <nums.Length; i++)
+//{
+//	if (nums[i]%2==1)
+//	{
+//		sum = sum + nums[i];
+//	}
+
+//}
+//Console.WriteLine(sum);
+
+
+
+//5)Verilmish arrayin icindeki cut ededlerin sayini tapin.
+
+
+//int[] nums = { 2, 5, 8, 4, 6, 10 };
+//int count = 0;
+//for (int i = 0; i < nums.Length; i++)
+//{
+//	if (nums[i]%2==0)
+//	{
+//		count++;
+
+//	}
+
+//}
+//Console.WriteLine(count);
+
